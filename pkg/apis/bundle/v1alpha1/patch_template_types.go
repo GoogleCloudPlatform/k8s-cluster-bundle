a3fa646336367516b6cc1d2f5a8ef668b2e812c7
Josh Hoak <jrhoak@gmail.com>
2019-02-01 17:09:57 -0800

    Add a patch template builder type + options schema (#148)
    * Add a patch template builder type + options schema
    
    This PR adds
    
    - A builder form of the PatchTemplate
    - A schema for the Patch Template options
    - Two schemas for the PatchTemplateBuilder (before/after)
    
    Note: The schema is the same used by CRDs, so we will reuse the same logic.
    
    * Add deep copy and fix build.
    
    * Add JSON annotations.

--- pkg/apis/bundle/v1alpha1/patch_template_types.go
+++ pkg/apis/bundle/v1alpha1/patch_template_types.go
@@ -15,6 +15,7 @@
 package v1alpha1
 
 import (
+	apiextensions "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -28,5 +29,30 @@ type PatchTemplate struct {
 	// Template is a template that creates a patch for a K8S object. In other
 	// words, a templated YAML blob that's meant to be applied via
 	// strategic-merge-patch. It's currently assumed to be a YAML go-template.
-	Template string
+	Template string `json:"template,omitempty"`
+
+	// OptionsSchema is the schema for the parameters meant to be applied to
+	// the patch template.
+	OptionsSchema *apiextensions.JSONSchemaProps `json:"optionsSchema,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// PatchTemplateBuilder contains configuration for creating patch templates.
+type PatchTemplateBuilder struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// Template is a template that creates a patch for a K8S object. In other
+	// words, a templated YAML blob that's meant to be applied via
+	// strategic-merge-patch. It's currently assumed to be a YAML go-template.
+	Template string `json:"template,omitempty"`
+
+	// BuildSchema is the schema for the parameters meant to be applied to
+	// the patch template.
+	BuildSchema *apiextensions.JSONSchemaProps `json:"buildSchema,omitempty"`
+
+	// TargetSchema is the schema for the parameters after the build-phase. This
+	// becomes the 'OptionsSchema' field.
+	TargetSchema *apiextensions.JSONSchemaProps `json:"targetSchema,omitempty"`
 }
