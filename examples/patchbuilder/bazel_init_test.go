aec476f6996ecc10b222727c13a47547414c33d4
Josh Hoak <jhoak@google.com>
2019-02-20 12:22:48 -0800

    Fix up some tests to make some imports/tooling happier.

--- examples/patchbuilder/bazel_init_test.go
+++ examples/patchbuilder/bazel_init_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package patchbuilder
+package patchbuilder_test
 
 import (
 	"github.com/GoogleCloudPlatform/k8s-cluster-bundle/pkg/testutil"
