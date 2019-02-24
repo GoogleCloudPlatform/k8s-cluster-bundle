77658f96813c08f86d119314ed397b329409a342
Josh Hoak <jrhoak@gmail.com>
2019-02-24 12:08:44 -0800

    Add ObjectTemplate type and generate codegen.
    Partial work on #201

--- pkg/apis/bundle/v1alpha1/zz_generated.register.go
+++ pkg/apis/bundle/v1alpha1/zz_generated.register.go
@@ -63,6 +63,7 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&ComponentList{},
 		&ComponentSet{},
 		&ComponentSetList{},
+		&ObjectTemplate{},
 		&PatchTemplate{},
 		&PatchTemplateBuilder{},
 		&Requirements{},
