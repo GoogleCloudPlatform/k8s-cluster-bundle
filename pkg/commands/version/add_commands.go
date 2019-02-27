b5b79063a28124c5635570b0b2e39d5307592d96
Josh Hoak <jrhoak@gmail.com>
2019-02-26 17:02:55 -0800

    Update bundlectl version and add the appropriate copyright headers.

--- pkg/commands/version/add_commands.go
+++ pkg/commands/version/add_commands.go
@@ -1,9 +1,25 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package version contains the version command.
 package version
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
+
 	"github.com/GoogleCloudPlatform/k8s-cluster-bundle/pkg/version"
+	"github.com/spf13/cobra"
 )
 
 func AddCommandsTo(root *cobra.Command) {
