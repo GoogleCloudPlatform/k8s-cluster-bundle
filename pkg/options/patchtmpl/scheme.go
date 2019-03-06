380742e5a58c1091c7947cd65b327744f2e372ca
Josh Hoak <jrhoak@gmail.com>
2019-03-05 19:15:19 -0800

    Harden/clarify the patching logic. (#217)
    - Update the Scheme to have more types.
    - Remove JSON patch for now and log an error.

--- pkg/options/patchtmpl/scheme.go
+++ pkg/options/patchtmpl/scheme.go
@@ -15,8 +15,12 @@
 package patchtmpl
 
 import (
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	extv1beta1 "k8s.io/api/extensions/v1beta1"
+	policyv1beta1 "k8s.io/api/policy/v1beta1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	storagev1 "k8s.io/api/storage/v1"
 	crdext "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
@@ -35,13 +39,13 @@ func init() {
 		}
 	}
 
+	must(appsv1.AddToScheme(k.KubeScheme))
 	must(corev1.AddToScheme(k.KubeScheme))
-	must(extv1beta1.AddToScheme(k.KubeScheme))
 	must(crdext.AddToScheme(k.KubeScheme))
-
-	// Don't register the extension scheme right now -- it's a very heavy dep and
-	// isn't really being used at the moment.
-	// must(apiextv1beta1.AddToScheme(k.KubeScheme))
+	must(extv1beta1.AddToScheme(k.KubeScheme))
+	must(policyv1beta1.AddToScheme(k.KubeScheme))
+	must(rbacv1.AddToScheme(k.KubeScheme))
+	must(storagev1.AddToScheme(k.KubeScheme))
 
 	defaultPatcherScheme = k
 }
