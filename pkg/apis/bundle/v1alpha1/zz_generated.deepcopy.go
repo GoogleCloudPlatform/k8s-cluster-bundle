9e9b89b22fcfead76d30556e74366d689515885d
Josh Hoak <jrhoak@gmail.com>
2019-03-19 10:00:26 -0700

    Fix ListMeta and add v1beta2 to known types

--- pkg/apis/bundle/v1alpha1/zz_generated.deepcopy.go
+++ pkg/apis/bundle/v1alpha1/zz_generated.deepcopy.go
@@ -177,7 +177,7 @@ func (in *ComponentExclude) DeepCopy() *ComponentExclude {
 func (in *ComponentList) DeepCopyInto(out *ComponentList) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta
-	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	out.ListMeta = in.ListMeta
 	if in.Items != nil {
 		in, out := &in.Items, &out.Items
 		*out = make([]Component, len(*in))
@@ -269,7 +269,7 @@ func (in *ComponentSet) DeepCopyObject() runtime.Object {
 func (in *ComponentSetList) DeepCopyInto(out *ComponentSetList) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta
-	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	out.ListMeta = in.ListMeta
 	if in.Items != nil {
 		in, out := &in.Items, &out.Items
 		*out = make([]ComponentSet, len(*in))
