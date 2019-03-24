665acee60211e162a704be77dd240c6e4a77bfa7
Josh Hoak <jrhoak@gmail.com>
2019-03-24 09:41:05 -0700

    Add a multi-applier applier
    This allows chaining together appliers. By default, options are first
    applied to go templates, and then patches are applied. This is a good
    default ordering because it means patches can be later applied to go
    templates.
    
    Fixes: #249

---
+++ pkg/options/multi/multi.go
@@ -0,0 +1,54 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package multi is an applier that performs multiple different applier
+// approaches.
+package multi
+
+import (
+	bundle "github.com/GoogleCloudPlatform/k8s-cluster-bundle/pkg/apis/bundle/v1alpha1"
+	"github.com/GoogleCloudPlatform/k8s-cluster-bundle/pkg/options"
+	"github.com/GoogleCloudPlatform/k8s-cluster-bundle/pkg/options/gotmpl"
+	"github.com/GoogleCloudPlatform/k8s-cluster-bundle/pkg/options/patchtmpl"
+)
+
+type applier struct {
+	appliers []options.Applier
+}
+
+// NewApplier creates a new multi applier instance.
+func NewApplier(appliers []options.Applier) options.Applier {
+	return &applier{appliers: appliers[:]}
+}
+
+// NewDefaultApplier creates a default multi-applier
+func NewDefaultApplier() options.Applier {
+	return NewApplier([]options.Applier{
+		gotmpl.NewApplier(),
+		patchtmpl.NewDefaultApplier(),
+	})
+}
+
+// ApplyOptions by going through the appliers in order.
+func (m *applier) ApplyOptions(comp *bundle.Component, opts options.JSONOptions) (*bundle.Component, error) {
+	comp = comp.DeepCopy()
+	for _, a := range m.appliers {
+		c, err := a.ApplyOptions(comp, opts)
+		if err != nil {
+			return nil, err
+		}
+		comp = c
+	}
+	return comp, nil
+}
