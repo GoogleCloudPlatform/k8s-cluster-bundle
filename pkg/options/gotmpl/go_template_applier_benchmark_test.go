dd38b62d203930002d59741f35f3ee811127bb79
BradfordMedeiros <bradmedeiros@google.com>
2019-05-01 15:26:14 -0700

    patcher benchmarks -- look fine!

--- pkg/options/gotmpl/go_template_applier_benchmark_test.go
+++ pkg/options/gotmpl/go_template_applier_benchmark_test.go
@@ -20,8 +20,8 @@ import (
 	"github.com/GoogleCloudPlatform/k8s-cluster-bundle/pkg/converter"
 )
 
-func BenchmarkGoTemplateApplier(t *testing.B){
-  component := `kind: Component
+func BenchmarkGoTemplateApplier(t *testing.B) {
+	component := `kind: Component
 spec:
   componentName: data-component
   objects:
@@ -52,23 +52,23 @@ spec:
         - name: logger
           image: '{{.AnotherContainerImage}}'`
 
-  templates := map[string]interface{}{
-    "DNSPolicy": "FooBarPolicy",
-    "ContainerImage":        "MyContainerImage",
-    "AnotherDNSPolicy":      "BlooBlarPolicy",
-    "AnotherContainerImage": "AnotherContainerImageVal",
-  }
+	templates := map[string]interface{}{
+		"DNSPolicy":             "FooBarPolicy",
+		"ContainerImage":        "MyContainerImage",
+		"AnotherDNSPolicy":      "BlooBlarPolicy",
+		"AnotherContainerImage": "AnotherContainerImageVal",
+	}
 
-  for i := 0; i < t.N; i++ {
-    comp, err := converter.FromYAMLString(component).ToComponent()
-    if err != nil {
-      t.Fatal(err)
-    }
+	for i := 0; i < t.N; i++ {
+		comp, err := converter.FromYAMLString(component).ToComponent()
+		if err != nil {
+			t.Fatal(err)
+		}
 
-    _, err = NewApplier().ApplyOptions(comp, templates)
-    if err != nil {
-      t.Fatal(err)
-    }  
-  }
-  
+		_, err = NewApplier().ApplyOptions(comp, templates)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
 }
